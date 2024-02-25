ee4da0e4821d383de02f6337f621ed7f802ac0fa
dpqls0356 <dpqls0356@gmail.com>
2024-02-25 12:47:13 +0900

    [챕터3] AppConfig 코드에서 new MemoryMemberRepository() 중복호출 제거

--- basic/src/main/java/springCorePrinciples/basic/AppConfig.java
+++ basic/src/main/java/springCorePrinciples/basic/AppConfig.java
@@ -1,6 +1,8 @@
 package springCorePrinciples.basic;
 
+import springCorePrinciples.basic.discount.DiscountPolicy;
 import springCorePrinciples.basic.discount.FixDiscountPolicy;
+import springCorePrinciples.basic.member.MemberRepository;
 import springCorePrinciples.basic.member.MemberService;
 import springCorePrinciples.basic.member.MemberServiceImpl;
 import springCorePrinciples.basic.member.MemoryMemberRepository;
@@ -9,9 +11,18 @@ import springCorePrinciples.basic.order.OrderServiceImpl;
 
 public class AppConfig {
     public static MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(getMemberRepository());
     }
+
+    //MemoryMemberRepository 중복 호출을 없앴다.
+    private static MemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
+    }
+
     public  static OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());
+        return new OrderServiceImpl((MemoryMemberRepository) getMemberRepository(),discountPolicy());
+    }
+    public static DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 }
